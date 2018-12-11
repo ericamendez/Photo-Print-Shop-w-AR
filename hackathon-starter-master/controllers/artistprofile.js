06d983af8e34a0956b72fc7f24549740598620b0
ericamendez <ecmendez25@gmail.com>
2018-12-11 17:05:33 -0500

    working artist and photo pages

--- hackathon-starter-master/controllers/artistprofile.js
+++ hackathon-starter-master/controllers/artistprofile.js
@@ -3,7 +3,7 @@ const User = require('../models/User');
 
 exports.getSearch = (req, res) => {
     User.find((err, artists) => {
-    console.log(artists)
+    // console.log(artists)
     res.render('search.ejs', {
         user: artists,
         title: 'Search Artist'
@@ -12,17 +12,29 @@ exports.getSearch = (req, res) => {
 };
 
 exports.getArtist = (req, res) => {
-    
-    res.render('artist.ejs', {
-        title: 'Artist Page'
-    });
+    User.findById(req.query.userId, (err, artist) => {
+    // User.find({_id: req.query.userId}, (err, artist) => {
+        Gallery.find({ email: artist.email}, (err, existingPhoto) => {
+            res.render('artist.ejs', {
+                photos: existingPhoto,
+                user: artist,
+                title: 'Artist Page'
+            });
+        });
+    })
 };
 
 exports.getPhoto = (req, res) => {
-    
-    res.render('photo.ejs', {
-        title: 'Photo Page'
-    });
+    Gallery.findById(req.query.photoId, (err, reqPhoto) => {
+        User.find({ email: reqPhoto.email}, (err, artist) => {
+            console.log(artist)
+            res.render('photo.ejs', {
+                user: artist,
+                photo: reqPhoto,
+                title: 'Photo Page'
+            });
+        });
+    })
 };
 
 exports.getAr = (req, res) => {
